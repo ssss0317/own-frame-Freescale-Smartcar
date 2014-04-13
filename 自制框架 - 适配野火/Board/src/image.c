b3f2dfe42b604a8e6d0cf69ecb759879e3dcefda
ssss0317 <espresso0726@gmail.com>
2014-04-13 20:05:17 +0800

    增加动态曝光

--- 自制框架 - 适配野火/Board/src/image.c
+++ 自制框架 - 适配野火/Board/src/image.c
@@ -23,7 +23,7 @@
 //定义存储接收CCD图像的数组
 extern uint8 CCD_BUFF[TSL1401_MAX*3][TSL1401_SIZE];
 
-void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval,uint8 * avgval*/);
+void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval*/,uint8 * avgval);
 void maxvar(uint8 *buf,uint16 len,uint8  maxval);
 // //用于记录最大差分值的点
 
@@ -33,13 +33,17 @@ void List_Array(uint8 *Array_list,uint8 LineKey);
 uint8 SmallPoint = 0;
 uint8 BigPoint = 0;
 uint8 MiddPoint = 0;
-uint8 def_diff_fazhi = 10;//差分最大值的预设值
+uint8 def_diff_fazhi = 15;//差分最大值的预设值
 
 //定义的三个用于存放边界点的数组
 uint8 Array_Small[Array_Size] = {0};
 uint8 Array_Big[Array_Size] = {0};
 uint8 Array_Midd[Array_Size] = {0};
 
+
+uint8  avg[TSL1401_SIZE];
+extern uint8 time;
+void CalculationExposureTime();//动态计算曝光时间
 
 
 /*!
@@ -58,6 +62,10 @@ int  get_img_error(void)
     
     Size_t imgsize={TSL1401_SIZE,1};            //图像大小
     Size_t size={TSL1401_SIZE,30};              //显示区域大小
+   
+    
+    
+   
     /**/
         //uint8  max[TSL1401_SIZE];
     	//uint8  avg[TSL1401_SIZE];
@@ -72,8 +80,10 @@ int  get_img_error(void)
        // LCD_wave_display(site2,size,(uint8 *)&CCD_BUFF[1],BIN_MAX,BLUE ); 
 
         //求波形差分
-        abs_diff((uint8 *)&CCD_BUFF[TSL1401_MAX+0],(uint8 *)&CCD_BUFF[0],TSL1401_SIZE/*,&max[1],&avg[1]*/);
+        abs_diff((uint8 *)&CCD_BUFF[TSL1401_MAX+0],(uint8 *)&CCD_BUFF[0],TSL1401_SIZE/*,&max[1]*/,&avg[0]);
        // abs_diff((uint8 *)&CCD_BUFF[TSL1401_MAX+1],(uint8 *)&CCD_BUFF[1],TSL1401_SIZE,&max[1],&avg[1]);
+        
+        CalculationExposureTime();//计算下一次曝光时间
         
         //LCD 显示差分图  
         //LCD_wave_display(site1,size,(uint8 *)&CCD_BUFF[TSL1401_MAX+0],max[0],GREEN);        
@@ -95,9 +105,27 @@ int  get_img_error(void)
 }
 
 
-/*
-	
-*/
+//****************************计算动态曝光时间*************************//
+
+
+void CalculationExposureTime()
+{
+    if(*avg > 128)
+    {
+        time--;
+    }
+    else if(*avg < 128)
+    {
+        time++;
+    }
+    else
+      ;
+    if(time<=1)
+      time =1;
+    else if(time>=20)
+      time=20;
+}
+
 
 void maxvar(uint8 *buf,uint16 len,uint8  maxval)
 {
@@ -114,24 +142,25 @@ void maxvar(uint8 *buf,uint16 len,uint8  maxval)
 
 
 /*!
- *  @brief      计算差分绝对值
+ *  @brief      计算差分
  *  @since      v5.0
  *  @note       野火差分法补充说明：差分最大值maxval 和 差分平均值avgval 这两个
                 参数是为了便于定义确定阈值而加入的，可删除。差分平均值，一般返回结果
                 都非常小，因此顶层用不上，建议删掉（此处保留是为了给大家验证）
  */
-void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval,uint8 * avgval*/)
+void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval*/,uint8 * avgval)
 {
     int8 tmp;
     
     uint8 max = 0;
     uint8 max2 = 0;
-    //uint32 sum = 0;
+    uint32 sum = 0;
    // uint32 sum2 = 0;
     uint16 lentmp = 0;
     uint16 lentmp2 = 64;
     while(lentmp < 63)                                  //得到左侧最大差分值           
     {
+        sum += src[lentmp];//将所有灰度值放入sum中，这里只有0~62
         tmp =src[lentmp+2] - src[lentmp];
         if(tmp > max )
         {
@@ -147,6 +176,8 @@ void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval,uint8 * avgval*/
     {;}
     else
     {
+      SmallPoint = 0;
+      /***********************************************
       //sum = sum / 63;
       if((src[0]+src[1])/2 < 128)                                  
       {
@@ -156,12 +187,14 @@ void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval,uint8 * avgval*/
       {
         SmallPoint = 0;
       }
+      *******************************************************/
     }
     
     
-    while(lentmp2 < 126)                             //得到右侧最大差分值  
+    while(lentmp2 < 127)                             //得到右侧最大差分值  
     {
-        tmp =src[lentmp2] - src[lentmp2+2];
+        sum +=src[lentmp2];//将所有灰度值放入sum中，这里只有64~125
+        tmp =src[lentmp2-2] - src[lentmp2];
         if(tmp > max2 )
         {
             BigPoint = lentmp2; 
@@ -175,6 +208,8 @@ void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval,uint8 * avgval*/
     {;}
     else
     {
+      BigPoint = 127;
+      /************************************
       //sum2 = sum2 / 63;
       if((src[126]+src[127])/2 < 128)             
       {
@@ -184,12 +219,14 @@ void abs_diff(uint8 *dst,uint8 *src,uint16 len/*,uint8 * maxval,uint8 * avgval*/
       {
         BigPoint = 127;
       }
+      *************************************/
     }
     *dst = 0;               // 最后一个 点配置为 0
     
     // *maxval = max;           // 返回最大绝对差值
-    // *avgval = (uint8)(sum/(len-1));  //前 len -1 个数的平均值
-    MiddPoint = (SmallPoint + BigPoint)/2;   
+    *avgval =(uint8)(sum/126);  //只有126个像素值的平均，用于调节动态曝光
+    MiddPoint = (SmallPoint + BigPoint)/2; 
+    printf("%d",MiddPoint);
 }
 
 
